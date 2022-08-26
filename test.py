49f87655eb422f4527ff28fd48e13d56b6c61b4c
chrystianobezerra <97260350+chrystianobezerra@users.noreply.github.com>
2022-08-26 15:56:31 -0300

    Teste de Falha

--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Olá eu sou o Goku! Estou demonstrando DevOps!!")
+        self.assertEqual(self.result.data.decode('utf-8'), "Olá eu sou o Goku!!!")
